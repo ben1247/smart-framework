53472b5fd6cd4f985f1afcb4dffa15816b78a8f6
张 悦 <yue.zhang@shuyun.com>
2017-11-01 11:04:07 +0800

    add upload model

--- src/main/java/org/smart4j/framework/DispatcherServlet.java
+++ src/main/java/org/smart4j/framework/DispatcherServlet.java
@@ -86,68 +86,6 @@ public class DispatcherServlet extends HttpServlet{
                 handleDataResult((Data)result,response);
             }
 
-//            // 创建请求参数对象
-//            Map<String,Object> paramMap = new HashMap<>();
-//            Enumeration<String> paramNames = request.getParameterNames();
-//            while (paramNames.hasMoreElements()){
-//                String paramName = paramNames.nextElement();
-//                String paramValue = request.getParameter(paramName);
-//                paramMap.put(paramName,paramValue);
-//            }
-//            String body = CodecUtil.decodeURL(StreamUtil.getString(request.getInputStream()));
-//            if(StringUtil.isNotEmpty(body)){
-//                String [] params = StringUtil.splitString(body,"&");
-//                if(ArrayUtil.isNotEmpty(params)){
-//                    for(String param : params){
-//                        String [] array = StringUtil.splitString(param,"=");
-//                        if(ArrayUtil.isNotEmpty(array) && array.length == 2){
-//                            String paramName = array[0];
-//                            String paramValue = array[1];
-//                            paramMap.put(paramName,paramValue);
-//                        }
-//                    }
-//                }
-//            }
-//            Param param = new Param(null,null);
-//            // 调用Action方法
-//            Method actionMethod = handler.getActionMethod();
-//            Object result;
-//            if (param.isEmpty()){
-//                result = ReflectionUtil.invokeMethod(controllerBean,actionMethod);
-//            }else {
-//                result = ReflectionUtil.invokeMethod(controllerBean,actionMethod,param);
-//            }
-//            // 处理Action方法返回值
-//            if(result instanceof View){
-//                // 返回JSP页面
-//                View view = (View)result;
-//                String path = view.getPath();
-//                if(StringUtil.isNotEmpty(path)){
-//                    if(path.startsWith("/")){
-//                        response.sendRedirect(request.getContextPath() + path);
-//                    }else{
-//                        Map<String,Object> model = view.getModel();
-//                        for (Map.Entry<String,Object> entry : model.entrySet()){
-//                            request.setAttribute(entry.getKey(),entry.getValue());
-//                        }
-//                        request.getRequestDispatcher(ConfigHelper.getAppJspPath() + path).forward(request,response);
-//                    }
-//                }
-//
-//            }else if(result instanceof Data){
-//                // 返回JSON数据
-//                Data data = (Data)result;
-//                Object model = data.getModel();
-//                if(model != null){
-//                    response.setContentType("application/json");
-//                    response.setCharacterEncoding("UTF-8");
-//                    PrintWriter writer = response.getWriter();
-//                    String json = JsonUtil.toJson(model);
-//                    writer.write(json);
-//                    writer.flush();
-//                    writer.close();
-//                }
-//            }
         }
     }
 
